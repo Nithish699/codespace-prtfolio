6507ad057f76ed0ab60b26d63fb11b331007b359
Nithish s <149039187+Nithish699@users.noreply.github.com>
2024-09-30 14:43:11 +0000

    Update name in siteProps object

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Nithish",
+  name: "Nithish_S",
   title: "Software Engineering Student",
   email: "nithish699734@gmail.com",
   gitHub: "Nithish699",
